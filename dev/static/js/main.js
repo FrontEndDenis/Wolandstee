94169b056df53c57ec9ce71ed78a266094f31517
FrontEndDenis <Lebedev.Denis.1996@mail.ru>
2020-10-16 18:36:53 +0500

    Корзина

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -14,7 +14,7 @@ function sliderInit() {
 			el: '.s-main-slider__pagination',
 			clickable: true,
 			renderBullet: function (index, className) {
-				return '<span class="' + className + '">' + (text[index]) + '</span>';
+				return `<span class="` + className + `">` + (text[index]) + `</span>`;
 			}
 		}
 	})
@@ -40,11 +40,26 @@ function isMobile() {
 }
 
 // Анимации
-function animation(item, anmClass) {
-	item.classList.add(anmClass);
-	item.addEventListener('animationend', () => {
-		item.classList.remove(anmClass);
-	}, { once: true });
+// function animation(item, addClass) {
+// 	item.classList.add(addClass);
+// 	item.addEventListener('animationend', () => {
+// 		item.classList.remove(addClass);
+// 	}, { once: true });
+// }
+function animateCSS(element, animation) {
+	return new Promise((resolve, reject) => {
+		const animationName = animation;
+		const node = (element.nodeType === 1) ? element : document.querySelector(element);
+
+		node.classList.add('animation', ...animationName.split(' '));
+
+		function handleAnimationEnd() {
+			node.classList.remove('animation', ...animationName.split(' '));
+			resolve('Animation ended');
+		}
+
+		node.addEventListener('animationend', handleAnimationEnd, { once: true });
+	});
 }
 
 // Валидация
@@ -68,12 +83,11 @@ function openSearchHeader() {
 	const btn = document.querySelector('#open-search-header'),
 		bottomLine = document.querySelector('.bottom-line'),
 		btnClose = btn.previousElementSibling.querySelector('.search-input__close'),
-		input =  btn.previousElementSibling.querySelector('input');
+		input = btn.previousElementSibling.querySelector('input');
 
 	const show = () => {
 		bottomLine.classList.add('open-search');
-		btn.previousElementSibling.classList.add('active');
-		animation(btn.previousElementSibling, 'fade-in');
+		btn.previousElementSibling.classList.add('active', 'animation', 'fade-in');
 	}
 
 	const hide = () => {
@@ -93,12 +107,11 @@ function catalogListInfo() {
 		block = elem.querySelector('.catalog-list__wrap');
 
 	const show = () => {
-		elem.classList.add('active');
-		animation(block, 'fade-in');
+		elem.classList.add('active', 'animation', 'fade-in');
 	}
 
 	const hide = () => {
-		elem.classList.remove('active');
+		elem.classList.remove('active', 'animation', 'fade-out');
 	}
 	elem.addEventListener('mouseenter', show, false);
 	elem.addEventListener('mouseleave', hide, false);
@@ -111,11 +124,11 @@ function catalogMenu() {
 		panel = document.querySelector('.bottom-line__navitagion-panel'),
 		elems = panel.querySelectorAll('li'),
 		block = document.querySelector('.bottom-line__catalog');
-	
+
 	const checkEl = item => {
-		if(item.classList.contains('active')) {
+		if (item.classList.contains('active')) {
 			addEl(item)
-		} else if(item.classList.contains('spacial')) {
+		} else if (item.classList.contains('spacial')) {
 			addEl(item)
 			block.classList.remove('active');
 		} else {
@@ -137,8 +150,7 @@ function catalogMenu() {
 	}
 
 	const show = () => {
-		block.classList.add('active');
-		animation(block, 'fade-in');
+		block.classList.add('active', 'animation', 'fade-in');
 	}
 
 	elems.forEach(elem => {
@@ -175,8 +187,188 @@ function tabs() {
 		navs.forEach((nav) => nav.classList.remove('active'));
 		contents.forEach((content) => content.classList.remove('active'));
 		navs[id].classList.add('active');
-		contents[id].classList.add('active')
-		animation(contents[id], 'fade-in-scale');
+		contents[id].classList.add('active', 'animation', 'fade-in-scale');
 	}
 }
 tabs();
+
+// Кнопки +- в карточке
+function inputNumber() {
+	const el = document.querySelectorAll('.input-number');
+
+	el.forEach((e) => {
+		const input = e.querySelector('.input-number__field'),
+			plus = e.querySelector('.input-number__plus'),
+			minus = e.querySelector('.input-number__minus');
+
+		const delStr = item => {
+			input.value = parseInt(item.replace(/[^\d]/g, ''));
+		}
+
+		const addUnits = item => {
+			return item += ' шт.'
+		}
+
+		if (input && plus && minus) {
+			plus.addEventListener('click', () => {
+				delStr(input.value);
+				if (input.max && Number(input.value) + 1 <= input.max) {
+					input.value = addUnits((Number(input.value) + 1));
+				}
+			});
+
+			minus.addEventListener('click', () => {
+				delStr(input.value);
+				(input.min && Number(input.value) - 1 >= input.min) ? input.value = addUnits((Number(input.value) - 1)) : input.value = 1 + ' шт.'
+
+			});
+
+			input.addEventListener('input', () => {
+				delStr(input.value);
+				input.value = input.value.replace(/[^0-9+]/, '');
+				if (input.max && Number(input.value) > input.max) {
+					input.value = input.max;
+				} else if (input.value === 'aN') {
+					input.value = '';
+				}
+			});
+			input.addEventListener('change', () => {
+				input.value = input.value + ' шт.';
+			});
+		}
+	});
+}
+inputNumber();
+
+
+// Корзина
+
+class Basket {
+	constructor(className) {
+		this.element = document.querySelector(className);
+	}
+
+	show(n, classElem) {
+		const sections = this.element.querySelectorAll(classElem);
+
+		this.hideAll(classElem);
+
+		sections[n].classList.add('show-section', 'animate__animated', 'fade-in');
+	}
+
+	hideAll(classElem) {
+		const sections = this.element.querySelectorAll(classElem);
+
+		sections.forEach((section) => {
+			section.classList.remove('show-section', 'animate__animated', 'fade-out');
+		});
+	}
+}
+
+const parent = document.querySelector('.modal-basket'),
+	section = parent.querySelectorAll('.modal-basket__section'),
+	item = parent.querySelectorAll('.modal-basket__basket-item'),
+	request = parent.querySelector('.modal-basket__request'),
+	clearList = parent.querySelector('.modal-basket__clear-list'),
+	yes = parent.querySelector('.modal-basket__yes'),
+	no = parent.querySelector('.modal-basket__no'),
+	prevBasket = parent.querySelector('.modal-basket__prev-basket'),
+	counter = parent.querySelector('.modal-basket__count-product'),
+	delCard = document.querySelectorAll('.basket-card__close');
+
+let count = 0,
+	countSec = 0;
+
+window.basket = new Basket('.modal-basket');
+
+
+
+function basketApp() {
+	document.addEventListener('changeBasket', (evt) => {
+		const cards = document.querySelectorAll('.basket-card');
+
+		counter.textContent = `${cards.length} товара`;
+
+		if (cards.length > 0) {
+			basket.show(countSec, '.modal-basket__section');
+			basket.show(count, '.modal-basket__basket-item');
+		} else {
+			basket.show(countSec, '.modal-basket__section');
+			basket.show(2, '.modal-basket__basket-item');
+			counter.textContent = `Нет товаров`;
+		}
+	});
+	basket.show(0, '.modal-basket__section')
+	basket.show(0, '.modal-basket__basket-item')
+	document.dispatchEvent(new Event('changeBasket'));
+}
+basketApp();
+
+clearList.addEventListener('click', () => {
+	count++;
+	basket.show(count, '.modal-basket__basket-item');
+});
+
+delCard.forEach((del) => {
+	del.addEventListener('click', (evt) => {
+		evt.preventDefault();
+
+		del.closest('.basket-card').remove();
+
+		document.dispatchEvent(new Event('changeBasket'));
+	});
+});
+
+yes.addEventListener('click', (evt) => {
+	evt.preventDefault();
+
+	const cards = document.querySelectorAll('.basket-card');
+
+	cards.forEach(card => card.remove());
+
+	document.dispatchEvent(new Event('changeBasket'));
+});
+
+no.addEventListener('click', () => {
+	count--;
+	basket.show(count, '.modal-basket__basket-item');
+
+	document.dispatchEvent(new Event('changeBasket'));
+});
+
+request.addEventListener('click', () => {
+	countSec++;
+	basket.show(countSec, '.modal-basket__section');
+})
+
+prevBasket.addEventListener('click', () => {
+	countSec--;
+	basket.show(countSec, '.modal-basket__section');
+});
+
+
+// Custom select
+function select()  {
+	let selectHeader = document.querySelectorAll('.select__header'),
+		selectItem = document.querySelectorAll('.select__item');
+	
+	selectHeader.forEach(item => {
+		item.addEventListener('click', selectToggle)
+	});
+	selectItem.forEach(item => {
+		item.addEventListener('click', selectChoose)
+	});
+	function selectToggle() {
+		this.parentElement.classList.toggle('active');
+	}
+	function selectChoose() {
+		let text = this.innerText,
+			select = this.closest('.select'),
+			currentText = select.querySelector('.select__text');
+		currentText.setAttribute('value', text)
+		currentText.innerText = text;
+		select.classList.remove('active');
+	}
+};
+
+select();
